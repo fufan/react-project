0ff02018e1a0683e432592f0e9c1762169f6ebce
fufan <>
2018-07-19 14:38:52 +0800

    初始化工程

---
+++ src/pages/home/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Button } from 'antd'
+// import { I18n } from 'react-i18next';
+// import styles from './home.scss';
+
+// import ReactDom from 'react-dom';
+
+// console.log(styles);
+function mapStateToProps(state) {
+    return state
+}
+
+@connect(mapStateToProps)
+export default class App extends Component {
+    static propTypes = {
+        test: PropTypes.number
+    }
+    constructor(props) {
+        super(props)
+        this.state = {
+            d: 11111
+        }
+    }
+    // const t2 = '123';
+    render() {
+        const { test } = this.props
+        return (
+            <div>
+                <Button>123</Button>
+                1231212{this.state.d}{test}
+            </div>
+        )
+    }
+}
+
+App.defaultProps = {
+    test: 1
+}
